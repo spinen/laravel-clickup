efd5e41c688119e5b3820f83711eb5ae360908a9
Stephen Finney <stephen.finney@spinen.com>
2019-11-25 14:52:33 -0500

    Formatting

--- database/migrations/2019_09_23_194855_add_click_up_token_to_users_table.php
+++ database/migrations/2019_09_23_194855_add_click_up_token_to_users_table.php
@@ -4,6 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * Class AddClickUpTokenToUsersTable
+ *
+ * Adds a column for the ClickUp API token to your users table.
+ */
 class AddClickUpTokenToUsersTable extends Migration
 {
     /**
